77e0237201b09a05aee8b1bc69a9d24597edea64
Humaira Afrin <humaira.afrin.stockholm@gmail.com>
2024-07-17 14:27:06 +0200

    update mini-project

---
+++ mipslab.h
@@ -0,0 +1,106 @@
+/* mipslab.h
+   Header file for all labs.
+   This file written 2015 by F Lundevall
+   Some parts are original code written by Axel Isaksson
+
+   Latest update 2015-08-28 by F Lundevall
+
+   For copyright and licensing, see file COPYING */
+
+/* Declare display-related functions from mipslabfunc.c */
+void display_image(int x, const uint8_t *data);
+void display_init(void);
+void display_string(int line, char *s);
+void display_update(void);
+uint8_t spi_send_recv(uint8_t data);
+
+/* Declare lab-related functions from mipslabfunc.c */
+extern const int decimalPosToBinary[8];
+void draw_pixel(int row, int col);
+void display_pixel(int row, int col, int val);
+void draw_icon(uint8_t* data_row, uint8_t* data_col, int size);
+void move_icon(uint8_t* data_row, uint8_t* data_col,int iconsize, int rowmovment, int colmovment);
+void move_titlescrean(uint8_t* data_row, uint8_t* data_col, int size, int rowmovement, int colmovment);
+
+char * itoaconv( int num );
+void labwork(void);
+int nextprime( int inval );
+void quicksleep(int cyc);
+void tick( unsigned int * timep );
+
+/* Declare display_debug - a function to help debugging.
+
+   After calling display_debug,
+   the two middle lines of the display show
+   an address and its current contents.
+
+   There's one parameter: the address to read and display.
+
+   Note: When you use this function, you should comment out any
+   repeated calls to display_image; display_image overwrites
+   about half of the digits shown by display_debug.
+*/
+void display_debug( volatile int * const addr );
+
+/* Declare bitmap array containing font */
+extern const uint8_t const font[128*8];
+/* Declare bitmap array containing icon */
+ uint8_t icon_row[20];
+uint8_t icon_col[20];
+/**** PIPES ***** */
+extern uint8_t pipe1_row[28];
+extern uint8_t pipe1_col[28];
+extern uint8_t pipe2_row[22];
+extern uint8_t pipe2_col[22];
+extern uint8_t pipe3_row[18];
+extern uint8_t pipe3_col[18];
+extern uint8_t pipe4_row[26];
+extern uint8_t pipe4_col[26];
+extern uint8_t pipe5_row[38];
+extern uint8_t pipe5_col[38];
+extern uint8_t pipe6_row[48];
+extern uint8_t pipe6_col[48];
+extern uint8_t pipe7_row[14];
+extern uint8_t pipe7_col[14];
+extern uint8_t pipe8_row[46];
+extern uint8_t pipe8_col[46];
+extern uint8_t pipe9_row[10];
+extern uint8_t pipe9_col[10];
+
+//extern const uint8_t const icon2[128];
+  uint8_t triangle[128*4];
+  uint8_t nybild[128*4];
+  uint8_t triangle1[128*4];
+    uint8_t triangle2[128*4];
+
+extern const uint8_t const obs1[128];
+extern const uint8_t const box[4*8];
+extern uint8_t gameover_row[235];
+extern uint8_t gameover_col[235];
+
+int gameState;
+extern countStart;
+int score;
+int highScore;
+
+
+int extern TacoX;       //Tacon som flyger
+int extern TacoY;
+
+int extern Tube1X;
+int extern Tube1Y;
+
+/* Declare text buffer for display output */
+extern char textbuffer[4][16];
+
+/* Declare functions written by students.
+   Note: Since we declare these functions here,
+   students must define their functions with the exact types
+   specified in the laboratory instructions. */
+/* Written as part of asm lab: delay, time2string */
+void delay(int);
+void time2string( char *, int );
+/* Written as part of i/o lab: getbtns, getsw, enable_interrupt */
+int getbtns(void);
+int getsw(void);
+void enable_interrupt(void);
---
+++ mipslabwork.c
@@ -0,0 +1,134 @@
+/* mipslabwork.c
+
+   This file written 2015 by F Lundevall
+   Updated 2017-04-21 by F Lundevall
+
+   This file should be changed by YOU! So you must
+   add comment(s) here with your name(s) and date(s):
+
+   This file modified 2017-04-31 by Ture Teknolog 
+
+   For copyright and licensing, see file COPYING */
+
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include "pic32mx.h"  /* Declarations of system-specific addresses etc */
+#include "mipslab.h"  /* Declatations for these labs */
+#include <stdio.h>
+
+
+int mytime = 0x5957;
+
+char textstring[] = "text, more text, and even more text!";
+volatile int *trisE;
+volatile int *portE;
+volatile int *portD;
+volatile int *triseD;
+volatile int *trisE = (volatile int*) 0xbf886100;
+volatile int *portE = (volatile int*) 0xbf886110;
+
+int timeoutcount = 0; 
+int count=0;
+int gamestate = 0;
+int box_move;
+int prime = 1234567;
+
+
+
+/* Interrupt Service Routine */
+void user_isr(void)
+{
+   if (IFS(0) & 0x0100)
+   { 
+      IFSCLR(0) = 0x100; 
+      timeoutcount++; 
+        if (timeoutcount == 10) // Every 10th interrupt
+        {
+            timeoutcount = 0;
+
+            switch (gamestate) 
+            {
+                case 0:
+                    clearScreen();
+                    draw_icon(icon_row, icon_col, 20);
+                    if (getbtns() == 4) {
+                        gamestate = 1;
+                    }
+                    break;
+
+                case 1:
+                    clearScreen();
+                    draw_icon(icon_row, icon_col, 20);
+                    draw_icon(pipe1_row,pipe1_col,28);
+                    draw_icon(pipe2_row,pipe2_col,22);
+                    draw_icon(pipe3_row,pipe3_col,18);
+                    draw_icon(pipe4_row,pipe4_col,26);
+                    draw_icon(pipe5_row,pipe5_col,38);  
+                    draw_icon(pipe7_row,pipe7_col,14);
+                    draw_icon(pipe8_row,pipe8_col,46);
+                    draw_icon(pipe9_row,pipe9_col,10);
+
+
+
+                    box_move = 2;
+                    if (getbtns() == 4) {
+                        box_move = -3;
+                    }
+                    move_icon(icon_row, icon_col, 20, box_move, 0);
+                    move_icon(pipe1_row, pipe1_col, 28, 0, -1);  // Example movement for pipe 1
+                    move_icon(pipe2_row, pipe2_col, 22, 0, -2);  // Example movement for pipe 2
+                    move_icon(pipe3_row, pipe3_col, 18, 0, -3);  // Example movement for pipe 3
+                    move_icon(pipe4_row, pipe4_col, 26, 0, -1);  // Example movement for pipe 4
+                    move_icon(pipe5_row, pipe5_col, 38, 0, -2);  // Example movement for pipe 5
+                    move_icon(pipe7_row, pipe7_col, 14, 0, -3);  // Example movement for pipe 7
+                    move_icon(pipe8_row, pipe8_col, 46, 0, -1);  // Example movement for pipe 8
+                    move_icon(pipe9_row, pipe9_col, 10, 0, -2);
+                    break;
+
+            }
+        }
+    }
+}
+
+ /*//SW3
+   if (IFS(0) & 0x8000) { 
+	   T2CONCLR = 0x8000;
+      IFSCLR(0) = 0x8000; 
+   }*/
+
+
+
+/* Lab-specific initialization goes here */
+void labinit(void)
+{
+  //Switch 4 interrupt
+  *trisE = 0x00 & *trisE;//Sets portE to output, LED lights
+  TRISDSET = 0x800; //Sets switch 4 to input
+  IPC(4) = 0x7 << 2 | 0x3; //Prioritet
+  IEC(0) = (IEC(0) | 0x80000); //Enable switch 4 interrupt
+
+
+  //Timer2
+  TMR2 = 0;          // Initalize timer 2
+  T2CONSET = 0x0070; // Set to prescale 1:256
+  PR2 = ((80000000 / 256) / 100);
+  T2CONSET = 0x8000;
+
+  IPCSET(2) = 6;
+  IECSET(0) = 0x100;
+
+  enable_interrupts();
+  return;
+}
+
+/* This function is called repetitively from the main program */
+void labwork( void )
+{   
+   prime = nextprime(prime);
+}
+
+  
+
+
+
+
+
